8a157bcec9063ccecf409978831fdad8dc7dc61a
kritika <kritikakaushik1346@gmail.com>
2024-06-25 12:50:50 +0530

    case 5

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,10 +2,34 @@
 
 class StringCalculator
     def self.add(input)
-      return 0 if input.empty?
-  
-      numbers = input.split(",").map(&:to_i)
-      numbers.sum
+        return 0 if input.empty?
+    
+        delimiter = extract_delimiter(input)
+        numbers = extract_numbers(input, delimiter)
+        validate_numbers(numbers)
+    
+        numbers.sum
+    end
+
+    private 
+    
+    def self.extract_delimiter(input)
+        delimiter = ","
+        if input.start_with?("//")
+          delimiter = input[2]
+          input = input.split("\n", 2).last
+        end
+        delimiter
+    end
+
+    def self.extract_numbers(input, delimiter)
+        input.gsub!(/\n/, delimiter)
+        input.split(delimiter).map(&:to_i)
+    end
+
+    def self.validate_numbers(numbers)
+        negatives = numbers.select { |num| num < 0 }
+        raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
     end
   end
   
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,5 +19,17 @@ RSpec.describe StringCalculator do
     it "returns the sum of multiple comma-separated numbers" do
       expect(StringCalculator.add("1,2,3")).to eq(6)
     end
+
+    it "handles new lines between numbers" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "supports different delimiters specified in the input" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it "raises an exception for negative numbers and shows all negatives in the message" do
+      expect { StringCalculator.add("-1,2,-3") }.to raise_error(RuntimeError, "Negative numbers not allowed: -1, -3")
+    end
   end
 end
