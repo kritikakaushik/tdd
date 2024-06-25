f66772684eb09451ec509e1171aa17a6e723ee79
kritika <kritikakaushik1346@gmail.com>
2024-06-25 12:36:21 +0530

    case 3

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,9 @@ class StringCalculator
     def self.add(input)
       return 0 if input.empty?
       return input.to_i if input.size == 1
+  
+      numbers = input.split(",").map(&:to_i)
+      numbers.sum
     end
-end
+  end
   
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,6 @@
 # spec/string_calculator_spec.rb
 
-require_relative '../string_calculator'
+require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe ".add" do
@@ -10,6 +10,10 @@ RSpec.describe StringCalculator do
 
     it "returns the number itself if there is only one number" do
       expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it "returns the sum of two comma-separated numbers" do
+      expect(StringCalculator.add("1,2")).to eq(3)
     end
   end
 end
